1650c3ebec91b4941d3e91ab4f418813a1fd2bb1
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-09-25 18:22:51 +0200

    Add link between CharacterForms and WeaponList

--- GameWeapon/Views/CharacterFormViewController.swift
+++ GameWeapon/Views/CharacterFormViewController.swift
@@ -8,28 +8,32 @@
 
 import UIKit
 
-class CharacterFormViewController: GWDefaultViewController {
+class CharacterFormViewController: GWDefaultViewController, WeaponDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = "New Character"
     }
+    
+    // MARK: - Weapon
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func chooseWeapon(sender: UIButton) {
+        let weaponView:WeaponListViewController = WeaponListViewController();
+        weaponView.delegate = self;
+        self.navigationController?.pushViewController(weaponView, animated: true);
+    }
+    
+    func didChooseWeapon(weapon:IWeapon) -> Void {
+        print("Choose weapon : " + weapon.name);
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        print("CharacterFormViewController -- prepareForSegue");
     }
-    */
 
 }
--- GameWeapon/Views/WeaponListViewController.swift
+++ GameWeapon/Views/WeaponListViewController.swift
@@ -8,28 +8,18 @@
 
 import UIKit
 
+protocol WeaponDelegate : NSObjectProtocol {
+    func didChooseWeapon(weapon:IWeapon) -> Void;
+}
+
 class WeaponListViewController: GWDefaultViewController {
+    
+    var delegate:WeaponDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.title = "Weapons";
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
